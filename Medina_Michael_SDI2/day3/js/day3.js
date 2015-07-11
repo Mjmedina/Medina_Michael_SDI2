318b4ae414b1523400e6eefa24a9302efff64e05
Mjmedina <mjmedina@fullsail.edu>
2015-07-11 18:03:16 -0400

    Arrays
    Array of Averages

--- Medina_Michael_SDI2/day3/js/day3.js
+++ Medina_Michael_SDI2/day3/js/day3.js
@@ -8,4 +8,20 @@ Assignment
 
 //alert("Just testing!");
 
- 
+ //Picking oranges
+
+ var orangeBins = [30,500,10002];
+
+ //How many oranges did we pick
+var totalOranges= orangeBins[0] + orangeBins[1] + orangeBins[2];
+
+console.log(totalOranges + " oranges were picked in 3 days");
+
+ //Average number of oranges picked
+ var averageOranges = totalOranges/3;
+
+ //length property of array
+ console.log(orangeBins.length);
+
+
+ console.log("The average number of oranges picked is"+averageOranges);
