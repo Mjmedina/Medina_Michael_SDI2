720903cbc0ce098d7b4ea41a8e9a2ad558813a3d
Mjmedina <mjmedina@fullsail.edu>
2015-07-11 20:23:07 -0400

    Castings and Parse
    Castings and Parse

--- Medina_Michael_SDI2/day3/js/day3_Prompts.js
+++ Medina_Michael_SDI2/day3/js/day3_Prompts.js
@@ -7,3 +7,27 @@ Prompts
  */
 
 //alert("Just testing!");
+
+ //Ask the user for their input
+ var userInput = prompt("Enter your year of birth");
+ console.log(userInput);
+
+var age = 2015-userInput;
+ console.log("You are "+age+" years old.");
+
+
+ //Area of a rectangle
+ //Prompt the user for width
+ var width = prompt("Let's calculate the area of a rectangle.\nPlease enter the width of the rectangle:");
+ var height = prompt("Please enter the height of the rectangle:");
+
+
+ //Calculate the area of the rectangle
+ var area = width*height;
+
+ //Tell user the answer
+ console.log("The area of the rectangle with a height of "+height+ " and a width of "+width+" is "+area+".");
+
+ var results = "The results of the area are "+area;
+
+ console.log(results);
