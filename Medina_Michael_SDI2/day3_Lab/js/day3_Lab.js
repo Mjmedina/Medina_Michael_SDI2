e78d8dc226bc45835e2a4dad71c6d47743b67063
Mjmedina <mjmedina@fullsail.edu>
2015-07-14 18:37:17 -0400

    Finish Acceleration Formula
    Vi-Vf/t=a

--- Medina_Michael_SDI2/day3_Lab/js/day3_Lab.js
+++ Medina_Michael_SDI2/day3_Lab/js/day3_Lab.js
@@ -13,9 +13,22 @@ alert("Let's find the acceleration of an object!");
 
  var array = [];
 
-//Prompt user for the information
+//Prompt user for the velocity.
  array.push(prompt("Enter the initial velocity on the object:"));
  array.push(prompt("Enter the final velocity on the object:"));
- array.push(prompt("Enter how much time has passed:"));
+
+ //Prompt the user for amount of time passed.
+ var time = (prompt("Enter how much time has passed:"));
 
  console.log(array);
+
+
+ var velocityArray = Number(array[0]) +- Number(array[1]);
+
+ //console.log(accelerationArray);
+
+ velocityArray /= time;
+ console.log("The acceleration on the object is "+ velocityArray + " m/s.");
+
+
+ 
