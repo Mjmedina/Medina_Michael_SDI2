37fef1eee01b1faa008e966d0c16d4a10ef2fae8
Mjmedina <mjmedina@fullsail.edu>
2015-07-14 17:28:02 -0400

    Variables of acceleration
    vInitial
    vFinal
    time

---
+++ Medina_Michael_SDI2/day3_Lab/js/day3_Lab.js
@@ -0,0 +1,17 @@
+
+ /*
+Michael Medina
+Date
+Section 00
+Assignment
+ */
+
+//alert("Just testing!");
+
+ 
+ //Acceleration on an object
+
+ var array = [];
+ var vInitial = prompt("Enter the initial velocity on the object:");
+ var vFinal = prompt("Enter the final velocity on the object:");
+ var time = prompt("Enter how much time has passed:");
