0660d1b62c517b166307f8379c6e367c31ddc404
Mjmedina <mjmedina@fullsail.edu>
2015-07-18 19:43:32 -0400

    loops
    l

--- Medina_Michael_SDI2/day5_lab/js/day5_lab.js
+++ Medina_Michael_SDI2/day5_lab/js/day5_lab.js
@@ -10,3 +10,13 @@ Conditionals Assignment
 
  var gamesPlayed = Number(prompt("We are going to figure out if you are addicted to video games.\nHow many hours did you play on day 1?"));
 
+ // var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+ if (gamesPlayed === "" || isNaN(gamesPlayed))
+ //Validating that user actually typed something in the box. Because users are often dumb.
+     while(gamesPlayed === "" || isNaN(gamesPlayed))
+     {
+         gamesPlayed = parseInt(prompt("You can't run from your problems. Answer the question.\nNow then, how many hours did you play video games for?"));
+     }
+
+
+  
