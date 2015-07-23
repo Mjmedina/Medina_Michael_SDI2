523695c45fb31c41400d192ea83b105c2c344db1
Mjmedina <mjmedina@fullsail.edu>
2015-07-23 18:02:23 -0400

    start day 8
    day 8

---
+++ Medina_Michael_SDI2/day6_lab/js/day6_lab.js
@@ -0,0 +1,22 @@
+
+ /*
+Michael Medina
+Date
+Section 00
+Assignment
+ */
+
+//alert("Just testing!");
+
+ var myNumber = 100;
+
+ while (myNumber > 15) {
+     console.log("My number is "+ myNumber);
+     //myNumber = myNumber + 1;
+     myNumber = 4;
+ }
+
+ // For loop
+ for(var i = 0; i <= 10; i++){
+     console.log(myNumber);
+ }
---
+++ Medina_Michael_SDI2/day8/js/day8.js
@@ -0,0 +1,27 @@
+
+ /*
+Michael Medina
+Date
+Section 00
+Assignment
+ */
+
+//alert("Just testing!");
+
+// //Basic comment structure
+// // Function function name(){//Code to run goes here.}
+//
+// //Create a function that will console.log().... something.
+//
+//function printHello(){
+//    //This code will run when the function runs.
+//    console.log("This function is working!");
+//
+//}
+//
+////Create a function call that will start our printHello function
+// //'Go Button' to the factory
+// printHello();
+//
+// printHello();
+// 
