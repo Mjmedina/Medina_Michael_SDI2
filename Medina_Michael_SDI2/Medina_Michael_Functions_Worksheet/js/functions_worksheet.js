0cb010170a15a396d0bd7d7291c7d38921b5a801
Mjmedina <mjmedina@fullsail.edu>
2015-07-23 20:39:48 -0400

    Start lab 8
    Create variable

---
+++ Medina_Michael_SDI2/Medina_Michael_Functions_Worksheet/js/functions_worksheet.js
@@ -0,0 +1,16 @@
+
+ /*
+Michael Medina
+Date
+Section 00
+Assignment
+ */
+
+//alert("Just testing!");
+
+
+                                    //Circumference of a circle!
+ //Prompt user
+ var rad = prompt ("What's the radius of the circle?");
+
+ 
