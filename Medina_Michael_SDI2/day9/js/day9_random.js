fed1e03118badb0b3755fe879ec6744a07cc098d
Mjmedina <mjmedina@fullsail.edu>
2015-07-25 17:38:06 -0400

    day9
    prompted and parsed the minimum value

--- Medina_Michael_SDI2/day9/js/day9_random.js
+++ Medina_Michael_SDI2/day9/js/day9_random.js
@@ -9,3 +9,16 @@ Assignment
 //alert("Just testing!");
 
 
+//Create a function that will take ina  user prompted max/min value and return a random number between them.
+
+ //Create a variable for the min value
+ var min = prompt("We are going to find a random number in between 2 values.\nWhat is the min value?");
+ //Validate that prompt
+ while(isNaN(min)|| min===""){
+     if(isNaN(min)){
+         min=prompt("Please only type in numbers.\nWhat is the min value?");
+     }else{
+         min=prompt("Please do not leave blank.\nWhat is the min value?");
+     }
+
+ } 
