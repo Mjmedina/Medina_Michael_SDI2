0c4474a0f8ddb7c1b354d062b0abf1d827693b7c
Mjmedina <mjmedina@fullsail.edu>
2015-07-28 18:34:45 -0400

    FunctionsAssignment
    Prom user for size of the tank in the vehicle.

--- Medina_Michael_SDI2/Medina_Michael_Functions_Assignment/js/Medina_Michael_Functions_Assignment.js
+++ Medina_Michael_SDI2/Medina_Michael_Functions_Assignment/js/Medina_Michael_Functions_Assignment.js
@@ -14,6 +14,19 @@ alert ("What's up Han Solo!!! We're gna make the Kessel Run today. Think you are
  while(travelMiles===""|| isNaN(travelMiles)){
      travelMiles= prompt("Gwwwwwwgrrruuuuurrrrr!!");
  }
+ var mT = prompt("Now tell me, how many miles do you have to travel?");
+ while(mT===""|| isNaN(mT)){
+     mT= prompt("Gwwwwwwgrrruuuuurrrrr!!");
+ }
+ //~~~~~~~~~~~~~
+ //FUNction time. the 'FUN" is right in the word...
+ function tTime(m,mG){
+     return mG/m;
+ }
+ //Promp the user for size of tank..... in the Falcoln
+ var galRem = prompt("Do you need some fancy space fuel to get there?\nHow many gallons are left in your tank?");
+ while(galRem===""|| isNaN(galRem)){
+     galRem= prompt("Gwwwwwwgrrruuuuurrrrr!!");
+ }
 
-
-  
+ 
